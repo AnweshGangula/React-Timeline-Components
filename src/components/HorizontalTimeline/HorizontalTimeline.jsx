1e03825473610c7f4a50584197b85477dd538aef
Gangula2 <gangulaanwesh@gmail.com>
2022-06-26 19:05:38 +0530

    Format all documents

--- src/components/HorizontalTimeline/HorizontalTimeline.jsx
+++ src/components/HorizontalTimeline/HorizontalTimeline.jsx
@@ -1,14 +1,15 @@
-import './HorizontalTimeline.css';
-import { useEffect } from 'react';
-
+import "./HorizontalTimeline.css";
+import { useEffect } from "react";
 
 export const HorizontalTimeline = (props) => {
   function TimelineElements(props) {
-    const entry = props.entry
+    const entry = props.entry;
     return (
-      <div className="timelineItem" style={{ "right": "0px" }}>
+      <div className="timelineItem" style={{ right: "0px" }}>
         <div className="flagContents">
-          <p className={entry.category}><b>{entry.title}</b></p>
+          <p className={entry.category}>
+            <b>{entry.title}</b>
+          </p>
           <p>{entry.name}</p>
           <p>{entry.duration}</p>
         </div>
@@ -16,33 +17,40 @@ export const HorizontalTimeline = (props) => {
           <div className={`flagPole ${entry.type}`}></div>
         </div>
       </div>
-    )
+    );
   }
 
-  let timeline = props.timelineData.map((entry, index) =>
+  let timeline = props.timelineData.map((entry, index) => (
     <TimelineElements key={index} entry={entry} />
-  )
+  ));
 
   useEffect(() => {
     let move = 0;
     var offset = 0;
-    const animate_timeline =  () => {
-      let timelineElements = document.getElementById("timeline-elements").childNodes;
-      timelineElements = [...timelineElements]
+    const animate_timeline = () => {
+      let timelineElements =
+        document.getElementById("timeline-elements").childNodes;
+      timelineElements = [...timelineElements];
       // timelineElements = timelineElements.reverse()
       if (move === 0) {
-        timelineElements.forEach(
-          function (currentValue, currentIndex, listObj) {
-            currentValue.style.right = offset + "px";
-            offset += 20;
-          });
+        timelineElements.forEach(function (
+          currentValue,
+          currentIndex,
+          listObj
+        ) {
+          currentValue.style.right = offset + "px";
+          offset += 20;
+        });
         offset = 0;
         move = 1;
       } else {
-        timelineElements.forEach(
-          function (currentValue, currentIndex, listObj) {
-            currentValue.style.right = "0px";
-          });
+        timelineElements.forEach(function (
+          currentValue,
+          currentIndex,
+          listObj
+        ) {
+          currentValue.style.right = "0px";
+        });
         move = 0;
       }
     };
@@ -51,17 +59,14 @@ export const HorizontalTimeline = (props) => {
     timelineDiv.onmouseenter = animate_timeline;
     timelineDiv.onmouseleave = animate_timeline;
     // timelineDiv.onmouseup = animate_timeline;
-
   });
   return (
-      <div id="parent-timeline" className="themeLight">
-        <div id="timeline-bar">
-          <p>🞀 Past</p>
-          <p>Present 🞂</p>
-        </div>
-        <div id="timeline-elements">
-          {timeline}
-        </div>
+    <div id="parent-timeline" className="themeLight">
+      <div id="timeline-bar">
+        <p>🞀 Past</p>
+        <p>Present 🞂</p>
       </div>
+      <div id="timeline-elements">{timeline}</div>
+    </div>
   );
-}
+};
